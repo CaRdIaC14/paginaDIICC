ab938f4c0cbc6579b420afe192e697e166b6c633
CaRdIaC14 <13jonatan12@gmail.com>
2024-04-01 19:45:49 -0300

    pagina oficial

---
+++ resources/views/include/head.php
@@ -0,0 +1,55 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php echo $pagetitle?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="img/logo/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/bootstrap.min.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/animate.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/meanmenu.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/magnific-popup.css");  ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/owl.carousel.min.css");  ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/font-awesome.min.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/et-line-icon.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/reset.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/ionicons.min.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/material-design-iconic-font.min.css"); ?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/style.css?v="); echo time();?>>
+    <link rel="stylesheet" href=<?php echo fromroot($file, "css/responsive.css"); ?>>
+    <script src=<?php echo fromroot($file, "js/vendor/modernizr-2.8.3.min.js"); ?>></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc2/css‌​/bootstrap-glyphicon‌​s.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/swipe.css">
+    <script  src="https://code.jquery.com/jquery-3.6.1.min.js" ></script>
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/baffle@0.3.6/dist/baffle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.3.67/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="css/app.css">
+
+    <link rel="stylessheet" href="css/wid1.css">
+
+    <script defer src="js/app.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/slider.css">
+
+    <link rel="stylesheet" href="css/academicoscards.css">
+    <script src="js/academicoscards.js"></script>
+
+    <link rel="stylesheet" href="css/galeriascards.css">
+
+    <link rel="stylesheet" href="css/estilos.css">
+    
+
+
+
+</head>
