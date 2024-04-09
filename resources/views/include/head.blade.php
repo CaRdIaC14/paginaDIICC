bfefb5e7e402da41e26edfe44f829c9c4f1d48e8
Ignacio <isfcog@gmail.com>
2024-04-09 18:41:22 -0400

    Arreglo Css

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -6,47 +6,47 @@
 
     <link rel="shortcut icon" href="img/logo/favicon.png">
     <!-- Place favicon.ico in the root directory -->
-    <link rel="stylesheet" href="css/bootstrap.min.css")>
-    <link rel="stylesheet" href="css/animate.css")>
-    <link rel="stylesheet" href="css/meanmenu.css")>
-    <link rel="stylesheet" href="css/magnific-popup.css") >
-    <link rel="stylesheet" href="css/owl.carousel.min.css") >
-    <link rel="stylesheet" href="css/font-awesome.min.css")>
-    <link rel="stylesheet" href="css/et-line-icon.css")>
-    <link rel="stylesheet" href="css/reset.css")>
-    <link rel="stylesheet" href="css/ionicons.min.css")>
-    <link rel="stylesheet" href="css/material-design-iconic-font.min.css")>
-    <link rel="stylesheet" href="css/style.css?v=")>
-    <link rel="stylesheet" href="css/responsive.css")>
+    <link rel="stylesheet" href="/css/bootstrap.min.css")>
+    <link rel="stylesheet" href="/css/animate.css")>
+    <link rel="stylesheet" href="/css/meanmenu.css")>
+    <link rel="stylesheet" href="/css/magnific-popup.css") >
+    <link rel="stylesheet" href="/css/owl.carousel.min.css") >
+    <link rel="stylesheet" href="/css/font-awesome.min.css")>
+    <link rel="stylesheet" href="/css/et-line-icon.css")>
+    <link rel="stylesheet" href="/css/reset.css")>
+    <link rel="stylesheet" href="/css/ionicons.min.css")>
+    <link rel="stylesheet" href="/css/material-design-iconic-font.min.css")>
+    <link rel="stylesheet" href="/css/style.css?v=")>
+    <link rel="stylesheet" href="/css/responsive.css")>
     <script src="js/vendor/modernizr-2.8.3.min.js")></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc2/css‌​/bootstrap-glyphicon‌​s.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/swipe.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/css/swipe.css">
     <script  src="https://code.jquery.com/jquery-3.6.1.min.js" ></script>
     <script  src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" ></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <script src="https://cdn.jsdelivr.net/npm/baffle@0.3.6/dist/baffle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.3.67/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 
-    <link rel="stylessheet" href="css/wid1.css">
+    <link rel="stylessheet" href="/css/wid1.css">
 
     <script defer src="js/app.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="/css/slider.css">
 
-    <link rel="stylesheet" href="css/academicoscards.css">
+    <link rel="stylesheet" href="/css/academicoscards.css">
     <script src="js/academicoscards.js"></script>
 
-    <link rel="stylesheet" href="css/galeriascards.css">
+    <link rel="stylesheet" href="/css/galeriascards.css">
 
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="/css/estilos.css">
     
 
 
